fc79537634ee5f88bb20fc82196842a9ecead3a4
anhntse <anhntse161193@gmail.com>
2014-04-02 10:10:13 +0700

    9
    9

--- happenings.php
+++ happenings.php
@@ -6,13 +6,27 @@
 </head>
 <style type="text/css">
 	#happenMenu{
-		color: white;
 		background-color: #118EC3;
 	}
 </style>
 <body>
-<div class="content">
-	<?php include 'header.php';?>
-</div>
+	<div class="content">
+		<?php include 'header.php';?>
+		<div id="ytplayer"></div>
+		<script>
+			var tag = document.createElement('script');
+			tag.src = "https://www.youtube.com/player_api";
+			var firstScriptTag = document.getElementsByTagName('script')[0];
+			firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+			var player;
+			function onYouTubePlayerAPIReady() {
+				player = new YT.Player('ytplayer', {
+					height: '390',
+					width: '640',
+					videoId: '4YMD6xELI_k'
+				});
+			}
+		</script>
+	</div>
 </body>
 </html>
