95182f5de63830d28796c070f74e5361a01e6e73
anhntse <anhntse161193@gmail.com>
2014-04-02 10:50:09 +0700

    1
    1

--- whatIsGroove.php
+++ whatIsGroove.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="src/style.css">
 </head>
 <style type="text/css">
 	#whatMenu{
@@ -17,6 +18,9 @@
 		</div>
 		<div class="row" align="center">
 			<a class="col-md-12"align="center">Upload your episode</a>
+		</div>
+		<div class="row" align="center">
+			<iframe width="50%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/142308793&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
 		</div>
 		<script>
 			var tag = document.createElement('script');
