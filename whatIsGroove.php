6065fe1454f9f95936d6199c459c98888326f978
anhntse <anhntse161193@gmail.com>
2014-04-03 12:34:44 +0700

    6
    6

--- whatIsGroove.php
+++ whatIsGroove.php
@@ -20,15 +20,6 @@
 		<div class="row" align="center">
 			<a class="col-md-12"align="center">Upload your episode</a>
 		</div>
-		<div>
-			<form id="fileupload" action="https://graph-video.facebook.com/me/videos" method="POST" enctype="multipart/form-data">
-				<input type="hidden" name="access_token" value="user_access_token">
-				<input type="text" name="title">
-				<input type="text" name="description">
-				<input type="file" name="file"> <!-- name must be file -->
-			</form>
-
-		</div>
 		<div class="row" align="center">
 			<iframe width="50%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/142308793&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
 		</div>
@@ -45,16 +36,6 @@
 					videoId: '4YMD6xELI_k'
 				});
 			}
-		</script>
-		<script type="text/javascript">
-			$('#fileupload').fileupload({
-				dataType: 'json',
-        		forceIframeTransport: true, //force use iframe or will no work
-        		autoUpload : true,
-		        //facebook book response will be send as param
-		        //you can use this page to save video (Graph Api) object on database
-		        redirect : 'http://pathToYourServer/videos?video=%s' 
-    		});
 		</script>
 	</div>
 </body>
