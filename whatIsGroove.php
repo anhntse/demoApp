0abb4a4013e3bc87029f885dc95d5eee49fe630f
anhntse <anhntse161193@gmail.com>
2014-04-03 14:50:09 +0700

    a
    a

--- whatIsGroove.php
+++ whatIsGroove.php
@@ -20,6 +20,39 @@
 		<div class="row" align="center">
 			<a class="col-md-12"align="center">Upload your episode</a>
 		</div>
+		<?php 
+		$app_id = "YOUR_APP_ID";
+		$app_secret = "YOUR_APP_SECRET"; 
+		$my_url = "YOUR_POST_LOGIN_URL"; 
+		$video_title = "YOUR_VIDEO_TITLE";
+		$video_desc = "YOUR_VIDEO_DESCRIPTION";
+
+		$code = $_REQUEST["code"];
+
+		if(empty($code)) {
+			$dialog_url = "http://www.facebook.com/dialog/oauth?client_id=" 
+			. $app_id . "&redirect_uri=" . urlencode($my_url) 
+			. "&scope=publish_stream";
+			echo("<script>top.location.href='" . $dialog_url . "'</script>");
+		}
+
+		$token_url = "https://graph.facebook.com/oauth/access_token?client_id="
+		. $app_id . "&redirect_uri=" . urlencode($my_url) 
+		. "&client_secret=" . $app_secret 
+		. "&code=" . $code;
+		$access_token = file_get_contents($token_url);
+		
+		$post_url = "https://graph-video.facebook.com/me/videos?"
+		. "title=" . $video_title. "&description=" . $video_desc 
+		. "&". $access_token;
+
+		echo '<form enctype="multipart/form-data" action=" '.$post_url.' "  
+		method="POST">';
+		echo 'Please choose a file:';
+		echo '<input name="file" type="file">';
+		echo '<input type="submit" value="Upload" />';
+		echo '</form>';
+		?>
 		<div class="row" align="center">
 			<iframe width="50%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/142308793&amp;auto_play=false&amp;hide_related=false&amp;visual=true"></iframe>
 		</div>
