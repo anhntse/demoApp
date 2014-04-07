ae9c37bd9906886ea9380f76314c83e5a08eb626
anhntse <anhntse161193@gmail.com>
2014-04-07 15:39:14 +0700

    ád
    sa

---
+++ gallery.php
@@ -0,0 +1,42 @@
+<style type="text/css">
+	#galleryMenu{
+		text-decoration: none;
+		background-color: #eeeeee;
+	}
+</style>
+
+<div class="content">
+	<h2>Gallery</h2>
+	<div class="row">
+		<div class="col-sm-4">
+			<div class="thumbnail">
+				<img data-src="" alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="thumbnail">
+				<img data-src="" alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="thumbnail">
+				<img data-src="" alt="...">
+				<div class="caption">
+					<h3>Thumbnail label</h3>
+					<p>...</p>
+					<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
--- singaporeDance.php
+++ singaporeDance.php
@@ -7,20 +7,6 @@
 
 <div class="content">
 	<div class="row" align="center">
-		<div id="ytplayer" class="col-md-12"></div>
+		<iframe width="560" height="315" src="//www.youtube.com/embed/MVt32qoyhi0" frameborder="0" allowfullscreen></iframe>
 	</div>
-	<script>
-		var tag = document.createElement('script');
-		tag.src = "https://www.youtube.com/player_api";
-		var firstScriptTag = document.getElementsByTagName('script')[0];
-		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-		var player;
-		function onYouTubePlayerAPIReady() {
-			player = new YT.Player('ytplayer', {
-				height: '500',
-				width: '100%',
-				videoId: '4YMD6xELI_k'
-			});
-		}
-	</script>
 </div>
