5c9ec9fdd146617ca542d3d8cf7184568a13877d
anhntse <anhntse161193@gmail.com>
2014-04-07 11:52:17 +0700

    as
    s

--- content.php
+++ content.php
@@ -2,9 +2,50 @@
 <html>
 <head>
 	<title></title>
+	<script src="libs/js/jquery.js"></script>
 </head>
 <body>
 <?php include 'header.php';?>
-
+<div id="section-content"></div>
+<script>
+	$function(){
+		$("#happenMenu").on('click', function(){
+			if($(this).hasClass("selected")){
+				return;
+			}
+			else{
+				$("#section-content").load("happenings.php");
+			}
+		});
+		$("#whatMenu").on('click', function(){
+			if($(this).hasClass("selected")){
+				return;
+			}
+			else{
+				$("#section-content").load("whatIsGroove.php");
+			}
+		});
+		$("#galleryMenu").on('click', function(){
+			if($(this).hasClass("selected")){
+				return;
+			}
+			else{
+				$("#section-content").load("gallery.php");
+			}
+		});
+		$("#singaporeMenu").on('click', function(){
+			if($(this).hasClass("selected")){
+				return;
+			}
+			else{
+				$("#section-content").load("singaporeDance.php");
+			}
+		});
+		$(".menu").on('click', function(){
+			$(".menu").removeClass("selected");
+            $(this).addClass("selected");
+		});
+	}
+</script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -6,13 +6,13 @@
 </head>
 <body>
 <div>
-	<div class="nav nav-pills" align="center">
-	  <div class="col-sm-2" id="happenMenu"><a href="happenings.php">Happenings</a></div>
-	  <div class="col-sm-2" id="whatMenu"><a href="whatIsGroove.php">What is Groove Nation?</a></div>
-	  <div class="col-sm-4"><h2>GROVE NATION</h2></div>
-	  <div class="col-sm-2" id="galleryMenu"><a href="gallery.php">Gallery</a></div>
-	  <div class="col-sm-2" id="singaporeMenu"><a href="singaporeDance.php">Singapore Dance Delight Vol5 </a></div>
-	</div>
+	<ul class="nav nav-pills" align="center">
+	  <li class="menu" id="happenMenu"><a href="happenings.php">Happenings</a></li>
+	  <li class="menu" id="whatMenu"><a href="whatIsGroove.php">What is Groove Nation?</a></li>
+	  <li><h2>GROVE NATION</h2></li>
+	  <li class="menu" id="galleryMenu"><a href="gallery.php">Gallery</a></li>
+	  <li class="menu" id="singaporeMenu"><a href="singaporeDance.php">Singapore Dance Delight Vol5 </a></li>
+	</ul>
 </div>
 </body>
 </html>
