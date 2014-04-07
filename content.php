cb9a73e87297256e09d109af8283953f776808b8
anhntse <anhntse161193@gmail.com>
2014-04-07 12:07:24 +0700

    asdsa
    asdsa

--- content.php
+++ content.php
@@ -6,17 +6,19 @@
 </head>
 <body>
 <!-- Header -->
-<div>
-	<ul class="nav nav-pills" align="center">
-	  <li class="menu" id="happenMenu"><a href="happenings.php">Happenings</a></li>
-	  <li class="menu" id="whatMenu"><a href="whatIsGroove.php">What is Groove Nation?</a></li>
-	  <li><h2>GROVE NATION</h2></li>
-	  <li class="menu" id="galleryMenu"><a href="gallery.php">Gallery</a></li>
-	  <li class="menu" id="singaporeMenu"><a href="singaporeDance.php">Singapore Dance Delight Vol5 </a></li>
-	</ul>
+<div style="position: relative">
+	<div>
+		<ul class="nav nav-pills" align="center">
+			<li class="menu" id="happenMenu"><a href="happenings.php">Happenings</a></li>
+			<li class="menu" id="whatMenu"><a href="whatIsGroove.php">What is Groove Nation?</a></li>
+			<li><h2>GROVE NATION</h2></li>
+			<li class="menu" id="galleryMenu"><a href="gallery.php">Gallery</a></li>
+			<li class="menu" id="singaporeMenu"><a href="singaporeDance.php">Singapore Dance Delight Vol5 </a></li>
+		</ul>
+	</div>
+	<!-- End Header -->
+	<div id="section-content"></div>
 </div>
-<!-- End Header -->
-<div id="section-content"></div>
 <script>
 	$(function(){
 		$("#happenMenu").on('click', function(){
