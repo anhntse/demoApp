7f1803ca384a4a656e62bb38492e676a34a3db08
anhntse <anhntse161193@gmail.com>
2014-04-08 10:00:26 +0700

    vvvád
    vvvvád

--- content.php
+++ content.php
@@ -4,7 +4,7 @@
 			<ul class="nav nav-pills" align="center">
 				<li class="menu" id="happenMenu"><a href="happenings.php">Happenings</a></li>
 				<li class="menu" id="whatMenu"><a href="whatIsGroove.php">What is Groove Nation?</a></li>
-				<li><h2>GROVE NATION</h2></li>
+				<li><img src="img/Logo.png"></li>
 				<li class="menu" id="galleryMenu"><a href="gallery.php">Gallery</a></li>
 				<li class="menu" id="singaporeMenu"><a href="singaporeDance.php">Singapore Dance Delight Vol5 </a></li>
 			</ul>
