892f00b72da6b9d0918dbcefff21172156583a34
anhntse <anhntse161193@gmail.com>
2014-04-08 10:46:30 +0700

    asd
    d

--- content.php
+++ content.php
@@ -2,11 +2,11 @@
 	<div style="position: relative">
 		<div class="header">
 			<ul class="nav nav-pills" align="center">
-				<li class="menu" id="happenMenu"><a href="happenings.php"><img src="menu1.png"></a></li>
-				<li class="menu" id="whatMenu"><a href="whatIsGroove.php"><img src="menu2.png"></a></li>
+				<li class="menu" id="happenMenu"><a href="happenings.php"><img src="img/menu/menu1.png"></a></li>
+				<li class="menu" id="whatMenu"><a href="whatIsGroove.php"><img src="img/menu/menu2.png"></a></li>
 				<li><img src="img/Logo.png"></li>
-				<li class="menu" id="galleryMenu"><a href="gallery.php"><img src="menu3.png"></a></li>
-				<li class="menu" id="singaporeMenu"><a href="singaporeDance.php"><img src="menu4.png"></a></li>
+				<li class="menu" id="galleryMenu"><a href="gallery.php"><img src="img/menu/menu3.png"></a></li>
+				<li class="menu" id="singaporeMenu"><a href="singaporeDance.php"><img src="img/menu/menu4.png"></a></li>
 			</ul>
 		</div>
 		<!-- End Header -->
