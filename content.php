b6c389432734fb0461c83f6ba9abd877ae5ce043
anhntse <anhntse161193@gmail.com>
2014-04-08 09:48:56 +0700

    as
    dc

--- content.php
+++ content.php
@@ -1,50 +1,52 @@
-<div style="position: relative">
-	<div>
-		<ul class="nav nav-pills" align="center">
-			<li class="menu" id="happenMenu"><a href="happenings.php">Happenings</a></li>
-			<li class="menu" id="whatMenu"><a href="whatIsGroove.php">What is Groove Nation?</a></li>
-			<li><h2>GROVE NATION</h2></li>
-			<li class="menu" id="galleryMenu"><a href="gallery.php">Gallery</a></li>
-			<li class="menu" id="singaporeMenu"><a href="singaporeDance.php">Singapore Dance Delight Vol5 </a></li>
-		</ul>
+<div class="main">
+	<div style="position: relative">
+		<div>
+			<ul class="nav nav-pills" align="center">
+				<li class="menu" id="happenMenu"><a href="happenings.php">Happenings</a></li>
+				<li class="menu" id="whatMenu"><a href="whatIsGroove.php">What is Groove Nation?</a></li>
+				<li><h2>GROVE NATION</h2></li>
+				<li class="menu" id="galleryMenu"><a href="gallery.php">Gallery</a></li>
+				<li class="menu" id="singaporeMenu"><a href="singaporeDance.php">Singapore Dance Delight Vol5 </a></li>
+			</ul>
+		</div>
+		<!-- End Header -->
+		<div id="section-content"></div>
 	</div>
-	<!-- End Header -->
-	<div id="section-content"></div>
+	<script>
+		$(function(){
+			$("#happenMenu").on('click', function(evt){
+				evt.preventDefault();
+				if($(this).hasClass("selected")){
+					return;
+				}
+				$("#section-content").load("happenings.php");
+			});
+			$("#whatMenu").on('click', function(evt){
+				evt.preventDefault();
+				if($(this).hasClass("selected")){
+					return;
+				}
+				$("#section-content").load("whatIsGroove.php");
+			});
+			$("#galleryMenu").on('click', function(evt){
+				evt.preventDefault();
+				if($(this).hasClass("selected")){
+					return;
+				}
+				$("#section-content").load("gallery.php");
+			});
+			$("#singaporeMenu").on('click', function(evt){
+				evt.preventDefault();
+				if($(this).hasClass("selected")){
+					return;
+				}
+				$("#section-content").load("singaporeDance.php");
+			});
+			$(".menu").on('click', function(evt){
+				evt.preventDefault();
+				$(".menu").removeClass("selected");
+				$(this).addClass("selected");
+			});
+		});
+	</script>
 </div>
-<script>
-	$(function(){
-		$("#happenMenu").on('click', function(evt){
-			evt.preventDefault();
-			if($(this).hasClass("selected")){
-				return;
-			}
-			$("#section-content").load("happenings.php");
-		});
-		$("#whatMenu").on('click', function(evt){
-			evt.preventDefault();
-			if($(this).hasClass("selected")){
-				return;
-			}
-			$("#section-content").load("whatIsGroove.php");
-		});
-		$("#galleryMenu").on('click', function(evt){
-			evt.preventDefault();
-			if($(this).hasClass("selected")){
-				return;
-			}
-			$("#section-content").load("gallery.php");
-		});
-		$("#singaporeMenu").on('click', function(evt){
-			evt.preventDefault();
-			if($(this).hasClass("selected")){
-				return;
-			}
-			$("#section-content").load("singaporeDance.php");
-		});
-		$(".menu").on('click', function(evt){
-			evt.preventDefault();
-			$(".menu").removeClass("selected");
-            $(this).addClass("selected");
-		});
-	});
-</script>
