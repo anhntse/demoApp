7644df2903fcd036f719f2549b54da1053ecf5bb
anhntse <anhntse161193@gmail.com>
2014-04-10 10:07:21 +0700

    vbcvc
    bcv

--- whatIsGroove.php
+++ whatIsGroove.php
@@ -40,7 +40,7 @@
 		}
 	}
 	?>
-	<div class="row" align="center">
+	<div class="row musicPlay" align="center">
 		<iframe width="50%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/140181705&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>
 	</div>
 
