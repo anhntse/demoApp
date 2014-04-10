340bceb1edcb56ae6076291fed8db949b6a216d5
anhntse <anhntse161193@gmail.com>
2014-04-10 10:23:23 +0700

    vbcvc
    bcv

--- singaporeDance.php
+++ singaporeDance.php
@@ -7,6 +7,6 @@
 </script>
 <div class="content">
 	<div class="row" align="center">
-		<iframe width="560" height="315" src="//www.youtube.com/embed/MVt32qoyhi0" frameborder="0" allowfullscreen></iframe>
+		<iframe width="100%" height="445" src="//www.youtube.com/embed/MVt32qoyhi0" frameborder="0" allowfullscreen></iframe>
 	</div>
 </div>
