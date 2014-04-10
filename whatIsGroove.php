6c8a878ed796a1ef315ad5de0209883fe7812d3c
anhntse <anhntse161193@gmail.com>
2014-04-10 15:27:19 +0700

    asd
    asd

--- whatIsGroove.php
+++ whatIsGroove.php
@@ -45,9 +45,9 @@
 			<div class="playMusic">
 				<iframe width="440" height="130" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/140181705&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>
 			</div>
-			<div class="downloadMusic">
+			<!-- <div class="downloadMusic">
 				<img src="img/upload/clickDownload.png">
-			</div>
+			</div> -->
 		</div>
 	</div>
 
